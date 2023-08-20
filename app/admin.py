0563c8295c4b0dc202752f4f7ad22354f3274d09
Sepehr Samarzadeh Vajdehfar <ssamarzadeh@gmail.com>
2023-08-21 00:53:18 +0330

    admin panel has been added

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from app.models import JobPost
 # Register your models here.
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title','date','salary')
+    list_filter = ('date','salary',)
+    search_fields = ['title']
+    fields = [('title','description'),'expiry']
+    
+
+admin.site.register(JobPost,JobAdmin)
